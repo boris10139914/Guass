a45ffba5b055cba19d68ab9cfccd5f5ddaee86fa
harris <harris2030057@gmail.com>
2017-11-10 15:00:43 +0800

    2017/11/10 guassv2

--- app/src/main/java/com/fju/guass/MainActivity.java
+++ app/src/main/java/com/fju/guass/MainActivity.java
@@ -12,34 +12,47 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     Random random =new Random();
-    int g =random.nextInt(9)+1;
+    int g =random.nextInt(10)+1;
     int g1 =5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bSend = findViewById(R.id.b_send);
     }
     public void guass(View view){
         EditText ednumber = findViewById(R.id.ed_number);
         int number =Integer.parseInt(ednumber.getText().toString());
-        if(number==g){
+        if(g<number){
+            Toast.makeText(MainActivity.this, "小一點", Toast.LENGTH_SHORT).show();
+        }
+        if(g>number){
+            Toast.makeText(MainActivity.this, "大一點", Toast.LENGTH_SHORT).show();
+        }
+        if(g==number){
+            Toast.makeText(MainActivity.this, "對了", Toast.LENGTH_SHORT).show();
+        }
+
+        /*if(number==g){
+
             new AlertDialog.Builder(this)
                     .setMessage("答對了")
                     .setTitle("結果")
                     .setNeutralButton("OK",null)
                     .show();
-        }if (number>g){
+        }
+        if (number>g){
             new  AlertDialog.Builder(this)
                     .setMessage("小一點")
                     .setTitle("結果")
                     .setNeutralButton("OK",null)
                     .show();
-        }if(number<g){
+        }
+        if(number<g){
             new  AlertDialog.Builder(this)
                     .setMessage("大一點")
                     .setTitle("結果")
                     .setNeutralButton("OK",null)
-                    .show();
+                    .show();*/
         }
     }
-}
