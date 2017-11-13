6f68dc17653b9a678b87df36551d2f5a95357d55
boris10139914 <boris556655@gmail.com>
2017-11-13 18:23:40 +0800

    GuessV3

--- app/src/main/java/com/fju/guass/MainActivity.java
+++ app/src/main/java/com/fju/guass/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
@@ -13,8 +14,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     Random random =new Random();
     int g =random.nextInt(10)+1;
-    int g1 =5;
-    int min=1,max=10;
+    int min=1,max=100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,26 +23,42 @@ public class MainActivity extends AppCompatActivity {
     }
     public void guass(View view){
         EditText ednumber = findViewById(R.id.ed_number);
-        int number =Integer.parseInt(ednumber.getText().toString());
+        int n =Integer.parseInt(ednumber.getText().toString());
        /* if(g<number){
 
             Toast.makeText(MainActivity.this, "小一點", Toast.LENGTH_SHORT).show();
         }*/
-        if(g>number){
-            min = number;
-            Toast.makeText(MainActivity.this, "請輸入:"+min+"~"+max, Toast.LENGTH_SHORT).show();
+        if(n>=max){
+
+            Toast.makeText(MainActivity.this, "請再次輸入:"+min+"~"+max, Toast.LENGTH_SHORT).show();
         }
-        if(g<number){
-            max = number;
-            Toast.makeText(MainActivity.this, "請輸入:"+min+"~"+max, Toast.LENGTH_SHORT).show();
+        if(n<=min){
+
+            Toast.makeText(MainActivity.this, "請再次輸入:"+min+"~"+max, Toast.LENGTH_SHORT).show();
         }
 
-        if(number==g){
+        if(n==g){
             new AlertDialog.Builder(this)
                     .setMessage("答對了")
                     .setTitle("結果")
                     .setNeutralButton("OK",null)
                     .show();
+        }
+        while (n>min&n<max&&n<g){
+            min= n;
+           Toast.makeText(this,"大 一點",Toast.LENGTH_SHORT).show();
+            TextView numbermin = findViewById(R.id.number_min);
+            //String s =String.valueOf(n);
+            numbermin.setText(Integer.toString(n));
+            break;
+        }
+        while (n>min&n<max&&n>g){
+            max= n;
+            Toast.makeText(this,"小 一點",Toast.LENGTH_SHORT).show();
+            TextView numbermax = findViewById(R.id.number_max);
+            //String s =String.valueOf(n);
+           numbermax.setText(Integer.toString(n));
+            break;
         }
       /*  if (number>g){
             max = number;
